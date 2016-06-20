5dadce73fe93ec55931e374d8abec93248524acd
Michel de Bree <michel@micheldebree.nl>
2016-06-20 08:11:38 +0200

    Checked in distributed artefacts.

--- dist.sh
+++ dist.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # brew install pandoc
 # brew cask install basictex
-pandoc -s --smart --reference-docx=reference.docx -o cv.docx README.md 
-pandoc -s --smart --to=odt -o cv.odt README.md 
-pandoc -s -o cv.html README.md 
-pandoc -s --smart -o cv.pdf README.md 
-pandoc -s --smart --to=epub -o cv.epub README.md 
-pandoc -s --to=plain -o cv.txt README.md 
+mkdir -p dist
+cp photo.jpg dist
+OUTPUTNAME="dist/michel de bree-cv"
+pandoc -s --smart --reference-docx=reference.docx -o "$OUTPUTNAME-en.docx" README.md 
+pandoc -s --smart --to=odt -o "$OUTPUTNAME-en.odt" README.md 
+pandoc -s -o "$OUTPUTNAME-en.html" README.md 
+pandoc -s --smart -o "$OUTPUTNAME-en.pdf" README.md 
+pandoc -s --smart --to=epub -o "$OUTPUTNAME-en.epub" README.md 
+pandoc -s --to=plain -o "$OUTPUTNAME-en.txt" README.md 
